3b2f6fbfa49c12c44216764fb376fc2e61d111d1
slow <171slow@gmail.com>
2025-07-29 20:32:57 +0000

    verifica se tá com instabilidade na api pix integrada, e ve se está demo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { cn } from '@/lib/utils';
@@ -6,11 +8,6 @@ import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
-export const metadata: Metadata = {
-  title: 'Centro de Recarga Free Fire',
-  description: 'O site oficial para comprar diamantes no Free Fire. Vários métodos de pagamento estão disponíveis para os jogadores do Brasil.',
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <head />
+      <head>
+        <title>Centro de Recarga Free Fire</title>
+        <meta name="description" content="O site oficial para comprar diamantes no Free Fire. Vários métodos de pagamento estão disponíveis para os jogadores do Brasil." />
+      </head>
       <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
         {children}
         <Toaster />
