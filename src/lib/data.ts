efbf1f863442e42b6165bf8502f55a8139e406d2
slow <171slow@gmail.com>
2025-08-05 18:33:50 +0000

    faça um novo banner https://contentgarena-a.akamaihd.net/GOP/newshop_ban

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -8,6 +8,10 @@ export const banners = [
         src: 'https://contentgarena-a.akamaihd.net/GOP/newshop_banners/77EA32E48F86033D.jpg?v=1753856459', 
         alt: 'Banner 5'
     },
+    { 
+        src: 'https://contentgarena-a.akamaihd.net/GOP/newshop_banners/47BED91C7ABCF1EA.png?v=1752155361', 
+        alt: 'Banner Novo' 
+    },
 ];
 
 export const diamondPacks = [
